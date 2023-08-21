76ef123257f069a40ddc9e7b63d00675d3dbb924
KevMcK <kmcrunk@gmail.com>
2023-08-21 17:37:06 -0500

    small updates

--- Server/server_training.py
+++ Server/server_training.py
@@ -22,13 +22,13 @@ if __name__ == '__main__':
         'metric': 'accuracy',
         'patience': 10,
         'epochs': 50,
-        'batch_size': 12}
+        'batch_size': 16}
 
     for model in model_types:
         # Train Model using all parameters
         Train_Detect_Model(dataset,
                            sample_lengths[0],
-                           feature_types[2],
+                           feature_types[0],
                            model,
                            specs,
                            testing_path,
