2c97bc0b92b0e6a79450fd5c028d477b55cb32a0
KevMcK <kmcrunk@gmail.com>
2023-09-28 10:17:57 -0500

    restructured to not repeat code

---
+++ Detection_Classification/Engine_Ambient/engine_ambient_train.py
@@ -0,0 +1,34 @@
+
+
+from Detection_Classification.CNN_Models.template_train_model import Train_Detect_Model
+
+from pathlib import Path
+
+
+if __name__ == '__main__':
+    dataset = Path('/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Data/ML Model Data/Engine vs Nothing/dataset')
+    testing_path = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Data/ML Model Data/Engine vs Nothing/test'
+
+    sample_lengths = [10, 8, 6, 4, 2]
+    feature_types = ['spectral', 'filter1', 'mfcc']
+    model_types = ['basic_1', 'basic_2', 'deep_1', 'deep_2']
+
+    specs = {
+        'test_size': 0.2,
+        'random_state': 42,
+        'l2_value': 0.01,
+        'optimizer': 'adam',
+        'loss': 'binary_crossentropy',
+        'metric': 'accuracy',
+        'patience': 5,
+        'epochs': 50,
+        'batch_size': 24}
+
+    Train_Detect_Model(dataset,
+                       sample_lengths[2],
+                       feature_types[2],
+                       model_types[0],
+                       specs,
+                       testing_path,
+                       load_data=True)
+
