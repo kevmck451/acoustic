2d125f038212bc7459668bc696bf361b76675896
KevMcK <kmcrunk@gmail.com>
2023-10-08 03:02:06 -0500

    updated text file metadata to save

--- Detection_Classification/CNN_Models/save_model.py
+++ Detection_Classification/CNN_Models/save_model.py
@@ -9,7 +9,8 @@ def save_model(model, model_type, feature, input_time, accuracy, specs, runtime,
     text_extension = '.txt'
     lib_dir = f'{Path.cwd()}/Prediction/model_library'
 
-    feature_params = kwargs.get('feature_params', 'None')
+    feature_params = kwargs.get('feature_params')
+    feature_shape = kwargs.get('feature_shape')
     if feature == 'spectral' and feature_params != 'None': feature_save_name = f'{feature_params[0]}-{feature_params[1]}'
     elif feature == 'mfcc' and feature_params != 'None': feature_save_name = f'{feature_params}'
     else: feature_save_name = 'None'
@@ -25,12 +26,12 @@ def save_model(model, model_type, feature, input_time, accuracy, specs, runtime,
     # Save text file with all info about model
     text_saveto = f'{lib_dir}/{feature}_{feature_save_name}_{str(input_time)}_{model_type}_{accuracy}_{str(index)}{text_extension}'
 
-    feature_params = kwargs.get('feature_params', 'None')
     with open(text_saveto, 'w') as f:
-        f.write('Model Type: ' + model_type + '\n')
+        f.write('Models Type: ' + model_type + '\n')
         f.write('Feature: ' + feature + '\n')
         f.write('Feature Parameters: ' + feature_save_name + '\n')
         f.write('Sample Length: ' + str(input_time) + 's\n')
+        f.write('Feature Shape:' + str(feature_shape) + '\n')
         f.write('Accuracy: ' + str(accuracy) + '%\n')
         f.write('Total Runtime: ' + str(runtime) + 's\n')
         f.write('Specs: ' + json.dumps(specs, indent=4) + '\n')
