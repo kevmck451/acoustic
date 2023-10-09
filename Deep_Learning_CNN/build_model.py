18b5bc6335b73dd6821cec7c4cc307247a4695b7
KevMcK <kmcrunk@gmail.com>
2023-10-08 19:01:09 -0500

    bring in all aspects of CNN to build final product

---
+++ Deep_Learning_CNN/build_model.py
@@ -0,0 +1,84 @@
+
+from Deep_Learning_CNN.load_features import load_features
+from Deep_Learning_CNN.create_models import create_model
+from Deep_Learning_CNN.train_model import train_model
+from Acoustic.utils import time_class
+
+
+from pathlib import Path
+import numpy as np
+
+
+
+
+def build_model(filepath, length, sample_rate, multi_channel, process_list, feature_type, feature_params,
+                conv_layers, dense_layers, l2_value, dropout_rate, activation,
+                test_size, random_state, optimizer, loss, metric, patience, epochs, batch_size):
+
+    timing_stats = time_class(name='Load Features')
+
+    features, labels = load_features(filepath,
+                                     length[2],
+                                     sample_rate[2],
+                                     multi_channel[0],
+                                     process_list,
+                                     feature_type[1],
+                                     feature_params)
+
+
+    # Create a flexible model
+    input_shape = features.shape
+    model = create_model(input_shape,
+                         conv_layers,
+                         dense_layers,
+                         l2_value,
+                         dropout_rate,
+                         activation)
+
+    # Train Model
+    train_model(features,
+                labels,
+                test_size,
+                random_state,
+                model, optimizer,
+                loss, metric,
+                patience,
+                epochs,
+                batch_size)
+
+    total_runtime = timing_stats.stats()
+
+
+
+if __name__ == '__main__':
+    filepath = Path('/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/'
+                    '1 Acoustic/Data/ML Model Data/Engine vs Ambience/dataset 2')
+    length = [2, 4, 6, 8, 10]
+    sample_rate = [12_000, 18_000, 24_000, 36_000, 48_000]
+    multi_channel = ['original', 'ch_1', 'ch_n', 'split_ch', 'mix_mono']
+    process_list = ['normalize']  # add labels to list in order to create new processing chain
+    feature_type = ['spectral', 'mfcc']
+    window_sizes = [256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536]
+    hop_sizes = [128, 256, 512, 1024]
+    # feature_params = {'bandwidth':(70, 5000), 'window_size':window_sizes[4], 'hop_size':hop_sizes[2]}  # Spectrum
+    feature_params = {'n_coeffs': 13}  # MFCC
+
+    conv_layers = [(32, (3, 3)), (64, (3, 3))]
+    dense_layers = [128, 64]
+    l2_value = 0.01
+    dropout_rate = 0.5
+    activation = 'relu'
+
+    test_size = 0.2,
+    random_state = 42,
+    optimizer = 'adam',
+    loss = 'binary_crossentropy',
+    metric = 'accuracy',
+    patience = 5,
+    epochs = 50,
+    batch_size = 24
+
+
+    build_model(filepath, length[2], sample_rate[2], multi_channel[0], process_list, feature_type[1], feature_params,
+                conv_layers, dense_layers, l2_value, dropout_rate, activation,
+                test_size, random_state, optimizer, loss, metric, patience, epochs, batch_size)
