0c6fe0a9152806205a391a6b2d2763aad5be44a7
KevMcK <kmcrunk@gmail.com>
2023-10-08 19:01:53 -0500

    created train function of CNN model

---
+++ Deep_Learning_CNN/train_model.py
@@ -0,0 +1,38 @@
+# file to train model on a dataset
+
+
+from Deep_Learning_CNN.load_features import load_features
+from Deep_Learning_CNN.create_models import create_model
+from Acoustic.utils import time_class
+
+
+from sklearn.model_selection import train_test_split
+
+from keras.callbacks import EarlyStopping
+from pathlib import Path
+import numpy as np
+
+
+
+
+
+def train_model(features, labels, test_size, random_state, model, optimizer, loss, metric, patience, epochs, batch_size):
+    print('Training Model')
+
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=test_size,
+                                                        random_state=random_state)
+    input_shape = (X_train.shape[1], X_train.shape[2], 1)
+
+    if features.shape != input_shape:
+        raise Exception('Input Shapes not as Expected')
+
+    # gc.collect()
+
+    model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
+    early_stopping = EarlyStopping(monitor='val_loss', patience=patience)
+    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test, y_test),
+              callbacks=[early_stopping])
+
+
+
+
