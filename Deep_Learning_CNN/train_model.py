e73abd9f20637ab33e1c672d023191789dac5464
KevMcK <kmcrunk@gmail.com>
2023-10-13 01:07:21 -0500

    added generator function to help with memory management

--- Deep_Learning_CNN/train_model.py
+++ Deep_Learning_CNN/train_model.py
@@ -1,35 +1,59 @@
 # file to train model on a dataset
 
 
-from Deep_Learning_CNN.load_features import load_features
-from Deep_Learning_CNN.create_models import create_model
-from Acoustic.utils import time_class
-
-
 from sklearn.model_selection import train_test_split
 
 from keras.callbacks import EarlyStopping
-from pathlib import Path
 import numpy as np
+import gc
 
 
+# def train_model(features, labels, test_size, random_state, model, optimizer, loss, metric, patience, epochs, batch_size):
+#     print('Training Model')
+#
+#     X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=test_size,
+#                                                         random_state=random_state)
+#
+#     gc.collect()
+#
+#     model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
+#     early_stopping = EarlyStopping(monitor='val_loss', patience=patience)
+#     model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test, y_test),
+#               callbacks=[early_stopping])
+#
+#
+#     return model
 
-
-
-def train_model(features, labels, test_size, random_state, model, optimizer, loss, metric, patience, epochs, batch_size):
+def train_model(features, labels, test_size, random_state, model, optimizer, loss, metric, patience, epochs,
+                batch_size):
     print('Training Model')
 
     X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=test_size,
                                                         random_state=random_state)
 
-
-    # gc.collect()
+    train_gen = data_generator(X_train, y_train, batch_size)
+    val_gen = data_generator(X_test, y_test, batch_size)
 
     model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
-    early_stopping = EarlyStopping(monitor='val_loss', patience=patience)
-    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test, y_test),
-              callbacks=[early_stopping])
 
+    early_stopping = EarlyStopping(monitor='val_loss', patience=patience)
+
+    steps_per_epoch = len(X_train) // batch_size
+    validation_steps = len(X_test) // batch_size
+
+    model.fit(train_gen,
+                steps_per_epoch=steps_per_epoch,
+                epochs=epochs,
+                validation_data=val_gen,
+                validation_steps=validation_steps,
+                callbacks=[early_stopping])
 
     return model
 
+
+# This is the data generator function
+def data_generator(X, y, batch_size=32):
+    while True:
+        for start in range(0, len(X), batch_size):
+            end = min(start + batch_size, len(X))
+            yield np.array(X[start:end]), np.array(y[start:end])
