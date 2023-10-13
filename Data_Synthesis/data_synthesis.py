aa0c855cac5b89ad5e54e6d5052cbc98d09fbdd7
KevMcK <kmcrunk@gmail.com>
2023-10-13 01:06:40 -0500

    edits plus breaking it into a function

--- Data_Synthesis/data_synthesis.py
+++ Data_Synthesis/data_synthesis.py
@@ -3,41 +3,29 @@
 from Acoustic.audio_abstract import Audio_Abstract
 import Acoustic.process as process
 
+from tqdm import tqdm as progress_bar
 import numpy as np
 from pathlib import Path
 import random
 
-if __name__ == '__main__':
 
-    sample_length = 11
-    noise_floor_level = 50
-    range_of_target_sound = (10, 60)
 
-    synthetic_directory = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Data/Synthetic'
 
-    # uav_options = ['hex', 'angel', 'penguin']
-    # noise_options = ['ambient', 'wind']
-    # target_options = ['engine', 'diesel', 'gas']
+def generate_synthetic_data(noise_floor_path, target_path, new_path, sample_length, sample_rate, noise_floor_level, range_of_target_sound):
+    Path(new_path).mkdir()
 
-    # What if I want to do this for multiple ambient environments?
-    noise_floor_path = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Data/Isolated Samples/Ambient/ag_amb_2-1.wav'
-    noise_floor = Audio_Abstract(filepath=noise_floor_path)
+    noise_floor = Audio_Abstract(filepath=noise_floor_path, sample_rate=sample_rate)
     # noise_floor.waveform()
 
-    noise_floor = process.normalize(noise_floor, percentage=100)
-    noise_floor = process.normalize(noise_floor, percentage=noise_floor_level)
     noise_floor_chunk_list, _ = process.generate_chunks(noise_floor, length=sample_length)
-    #
-    target_path = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Data/Isolated Samples/Diesel'
-    # target_path = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Data/Isolated Samples/Gas'
 
-    for file in Path(target_path).iterdir():
+    for file in progress_bar(Path(target_path).iterdir()):
         if 'wav' in file.suffix:
-            target = Audio_Abstract(filepath=file, sample_rate=24_000)
+            target = Audio_Abstract(filepath=file, sample_rate=sample_rate)
 
-            if target.sample_length is None or target.sample_length < sample_length:
-                print(file)
-                print('Go to Next Target Sample')
+            if target.sample_length is None:  # or target.sample_length < sample_length
+                # print(file)
+                # print('Go to Next Target Sample')
                 continue
             if target.num_channels > 1:
                 channel_list = process.channel_to_objects(target)
@@ -47,14 +35,37 @@ if __name__ == '__main__':
             normalization_values = list(np.arange(range_of_target_sound[0], range_of_target_sound[1], 2))
 
             for value in normalization_values:
-                target = process.normalize(target_chunk_list[random.randint(0, len(target_chunk_list) - 1)], percentage=100)
+                target = process.normalize(target_chunk_list[random.randint(0, len(target_chunk_list) - 1)],
+                                           percentage=100)
                 target = process.normalize(target, percentage=value)
+                noise_floor_sample = noise_floor_chunk_list[random.randint(0, len(noise_floor_chunk_list) - 1)]
+                noise_floor_edit = process.normalize(noise_floor_sample, percentage=100)
+                noise_floor_edit = process.normalize(noise_floor_edit, percentage=noise_floor_level)
 
-                mix = process.mix_to_mono(target, noise_floor_chunk_list[random.randint(0, len(noise_floor_chunk_list)-1)])
+                mix = process.mix_to_mono(target, noise_floor_edit)
                 mix.name = f'{target.name}_{noise_floor.name}_mix'
 
-                mix.export(filepath = synthetic_directory, name = f'{mix.name}_{value}')
+                mix.export(filepath=new_path, name=f'{mix.name}_{value}')
 
+
+if __name__ == '__main__':
+
+    noise_floor_path = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Data/Isolated Samples/Ambient/ag_amb_2-1.wav'
+    # target_path = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Data/Isolated Samples/Diesel'
+    target_path = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Data/Isolated Samples/Gas'
+
+    synthetic_directory = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Data/Synthetic'
+    new_path = synthetic_directory + '/gas_mix_2'
+    sample_length = 8
+    sample_rate = 24_000
+    noise_floor_level = 50
+    range_of_target_sound = (10, 60)
+
+    # uav_options = ['hex', 'angel', 'penguin']
+    # noise_options = ['ambient', 'wind']
+    # target_options = ['engine', 'diesel', 'gas']
+
+    generate_synthetic_data(noise_floor_path, target_path, new_path, sample_length, sample_rate, noise_floor_level, range_of_target_sound)
 
 
 
