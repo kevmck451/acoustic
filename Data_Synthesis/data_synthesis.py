0a8ab05c069175bd6b18eaa12a20164096e81d83
KevMcK <kmcrunk@gmail.com>
2023-10-19 11:33:46 -0500

    adjusted bug for missing name

--- Data_Synthesis/data_synthesis.py
+++ Data_Synthesis/data_synthesis.py
@@ -19,31 +19,26 @@ def generate_synthetic_data(noise_floor_path, target_path, new_path, sample_leng
     for file in progress_bar(Path(target_path).iterdir()):
         if 'wav' in file.suffix:
             target = Audio_Abstract(filepath=file, sample_rate=sample_rate)
-            print(target)
+
             if target.sample_length is None:  # or target.sample_length < sample_length
-                # print(file)
-                # print('Go to Next Target Sample')
                 continue
             if target.num_channels > 1:
                 channel_list = process.channel_to_objects(target)
                 target_new = process.mix_to_mono(channel_list)
                 target_new.path = target.path
+                target_new.name = target.name
                 target = target_new
             target_chunk_list, _ = process.generate_chunks(target, length=sample_length)
-            print(target_chunk_list[0])
+
             normalization_values = list(np.arange(range_of_target_sound[0], range_of_target_sound[1], range_of_target_sound[2]))
 
             for value in normalization_values:
-                target = process.normalize(target_chunk_list[random.randint(0, len(target_chunk_list) - 1)],
-                                           percentage=100)
+                target = process.normalize(target_chunk_list[random.randint(0, len(target_chunk_list) - 1)], percentage=100)
                 target = process.normalize(target, percentage=value)
-                print(target)
                 noise_floor_sample = noise_floor_chunk_list[random.randint(0, len(noise_floor_chunk_list) - 1)]
                 noise_floor_edit = process.normalize(noise_floor_sample, percentage=100)
                 noise_floor_edit = process.normalize(noise_floor_edit, percentage=noise_floor_level)
-                print(target)
                 mix = process.mix_to_mono([target, noise_floor_edit])
-                print(target)
                 mix.name = f'{target.name}_{noise_floor.name}_mix'
 
                 mix.export(filepath=new_path, name=f'{mix.name}_{value}')
@@ -51,11 +46,11 @@ def generate_synthetic_data(noise_floor_path, target_path, new_path, sample_leng
 if __name__ == '__main__':
     noise_floor_path = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Data/Isolated Samples/Ambient/residential_amb_2-1_clip.wav'
     synthetic_directory = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Data/Synthetic'
-    range_of_target_sound = (10, 80, 2)
-    noise_floor_level = 80
+    range_of_target_sound = (11, 71, 2)
+    noise_floor_level = 90
     sample_rate = 24_000
     sample_length = 8
-    mix_num = 3
+    mix_num = 4
 
     # Diesel Samples
     target_path = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Data/Isolated Samples/Diesel'
