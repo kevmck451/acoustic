383f21c4e7616ca18833efc361a2243325f158b3
KevMcK <kmcrunk@gmail.com>
2023-11-09 11:02:59 -0600

    initial commit

---
+++ Targets/target_class.py
@@ -0,0 +1,31 @@
+# File to characterize a sound source called "Target"
+
+from sample_library import *
+from utils import CSVFile
+
+import numpy as np
+import math
+import ast
+
+
+class Target:
+
+    def __init__(self):
+        self.name = str
+        self.type = str
+
+
+    def __str__(self):
+        return f'---------Target Object---------\n' \
+               f'Name: {self.name}\n' \
+               f'Type: {self.type}\n' \
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    deocy = Target()
