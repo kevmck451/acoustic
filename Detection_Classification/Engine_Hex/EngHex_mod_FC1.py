825cd009b9b208bc330c9e29e8d6e80fce73379d
KevMcK <kmcrunk@gmail.com>
2023-12-11 01:57:48 -0600

    organizational updates

--- Detection_Classification/Engine_Hex/EngHex_mod_FC1.py
+++ Detection_Classification/Engine_Hex/EngHex_mod_FC1.py
@@ -5,7 +5,7 @@ from Deep_Learning_CNN.build_model import build_model
 if __name__ == '__main__':
 
     filepath = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/' \
-               '1 Acoustic/Data/ML Model Data/Engine vs Hex/dataset 1'
+               '1 Acoustic/Data/ML Model Data/Engine vs Hex/dataset 3'
 
     # Loading Features
     length = [2, 4, 6, 8, 10]
@@ -29,11 +29,11 @@ if __name__ == '__main__':
     optimizer = 'adam'
     loss = 'binary_crossentropy'
     metric = 'accuracy'
-    patience = 8
-    epochs = 50
+    patience = 20
+    epochs = 100
     batch_size = 48
 
-    build_model(filepath, length[2], sample_rate[2], multi_channel[0], chunk_type, process_list, feature_type, feature_params,
+    build_model(filepath, length[0], sample_rate[2], multi_channel[1], chunk_type, process_list, feature_type, feature_params,
                     conv_layers, dense_layers, l2_value, dropout_rate, activation,
                     test_size, random_state, optimizer, loss, metric, patience, epochs, batch_size)
 
