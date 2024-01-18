77fce945b1d26389a497d3705e6fdd3da9aaa8a5
KevMcK <kmcrunk@gmail.com>
2024-01-18 17:55:57 -0600

    organizational edits

--- __init__.py
+++ __init__.py
@@ -5,21 +5,5 @@ Author: Kevin McKenzie
 Version: 1.0.0
 Start Date: May 2023
 ===============
-The Acoustic module provides a comprehensive set of tools for analyzing, processing, and visualizing audio data.
-
-Classes:
-- Audio: Analyzes and processes single-channel WAV files.
-- Audio_MC: Extends Audio class to support multi-channel audio files.
-- Samp_Lib: Manages user-specific sample library and metadata.
-- Compare: Provides methods for comparing audio samples.
-- Mount_Compare: Provides methods for comparing audio samples using microphone mounts.
-
-Modules:
-- Process: Contains functions for audio processing tasks.
-- Samples: Handles user-specific sample library and metadata.
-- Utils: Provides utility functions for working with files and directories.
-- Visualize: Includes functions for visualizing audio data.
-- Comparisons: Contains classes for comparing audio samples.
-- Mic_Mount: Defines classes for microphone mounts.
 """
 
