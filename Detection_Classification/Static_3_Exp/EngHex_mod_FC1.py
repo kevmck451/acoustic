ab67ce16df01ac689efda46c0570507512b6a85f
KevMcK <kmcrunk@gmail.com>
2024-01-20 02:51:05 -0600

    updated structure

--- Detection_Classification/Static_3_Exp/EngHex_mod_FC1.py
+++ Detection_Classification/Static_3_Exp/EngHex_mod_FC1.py
@@ -1,44 +1,31 @@
 
 from Investigations.DL_CNN.build_model import build_model
+import Static3_mod_config as model_config
 
 if __name__ == '__main__':
 
-    filepath = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/' \
-               '1 Acoustic/Data/ML Model Data/Static Test 3/dataset 1'
-
-    # Loading Features
-    length = [10, 20, 30, 40, 50]
-    sample_rate = [12_000, 18_000, 24_000, 36_000, 48_000]
-    multi_channel = ['original', 'ch_1', 'ch_n', 'split_ch', 'mix_mono']
-    chunk_type = ['regular', 'window']
-    process_list = ['normalize']  # add labels to list in order to create new processing chain
     feature_type = 'feature_combo_1'
     feature_params = 'None'
 
-    # Create Model
-    conv_layers = [(32, (3, 3)), (64, (3, 3))]
-    dense_layers = [256, 128]
-    l2_value = 0.01
-    dropout_rate = 0.5
-    activation = 'relu' # 'elu' or 'relu'
-
-    # Train Model
-    test_size = 0.2
-    random_state = 42
-    optimizer = 'adam'
-    loss = 'binary_crossentropy'
-    metric = 'accuracy'
-    patience = 8
-    epochs = 100
-    batch_size = 48
-
-    # to run model just once
-    # build_model(filepath, length[0], sample_rate[2], multi_channel[1], chunk_type, process_list, feature_type, feature_params,
-    #                 conv_layers, dense_layers, l2_value, dropout_rate, activation,
-    #                 test_size, random_state, optimizer, loss, metric, patience, epochs, batch_size)
-
-# to run models for different time
-    for len_val in length:
-        build_model(filepath, len_val, sample_rate[2], multi_channel[1], chunk_type, process_list, feature_type, feature_params,
-                    conv_layers, dense_layers, l2_value, dropout_rate, activation,
-                    test_size, random_state, optimizer, loss, metric, patience, epochs, batch_size)
+    for len_val in model_config.length:
+        build_model(model_config.filepath,
+                    len_val,
+                    model_config.sample_rate,
+                    model_config.multi_channel,
+                    model_config.chunk_type,
+                    model_config.process_list,
+                    feature_type,
+                    feature_params,
+                    model_config.conv_layers,
+                    model_config.dense_layers,
+                    model_config.l2_value,
+                    model_config.dropout_rate,
+                    model_config.activation,
+                    model_config.test_size,
+                    model_config.random_state,
+                    model_config.optimizer,
+                    model_config.loss,
+                    model_config.metric,
+                    model_config.patience,
+                    model_config.epochs,
+                    model_config.batch_size)
