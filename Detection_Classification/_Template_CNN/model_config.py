0e6314ba3fd8d125f950751ca1b89eea709c452c
KevMcK <kmcrunk@gmail.com>
2024-01-20 02:50:53 -0600

    added model config link

---
+++ Detection_Classification/_Template_CNN/model_config.py
@@ -0,0 +1,36 @@
+
+
+
+filepath = ''
+
+# Loading Features
+length = [10, 20, 30, 40, 50]
+
+# [12_000, 18_000, 24_000, 36_000, 48_000]
+sample_rate = 24_000
+
+# ['original', 'ch_1', 'ch_n', 'split_ch', 'mix_mono']
+multi_channel = 'ch_1'
+
+# ['regular', 'window']
+chunk_type = 'window'
+
+# add labels to list in order to create new processing chain
+process_list = ['normalize']
+
+# Create Model
+conv_layers = [(32, (3, 3)), (64, (3, 3))]
+dense_layers = [256, 128]
+l2_value = 0.01
+dropout_rate = 0.5
+activation = 'relu'
+
+# Train Model
+test_size = 0.2
+random_state = 42
+optimizer = 'adam'
+loss = 'binary_crossentropy'
+metric = 'accuracy'
+patience = 8
+epochs = 100
+batch_size = 48
