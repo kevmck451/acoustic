330033b356e299bd3e0b754deb70bd1f96c71251
KevMcK <kmcrunk@gmail.com>
2024-01-20 02:58:07 -0600

    updated naming

--- Detection_Classification/Static_3_Exp/Static3_mod_testing.py
+++ Detection_Classification/Static_3_Exp/Static3_mod_testing.py
@@ -1,33 +1,28 @@
 
-from Investigations.DL_CNN.predict import make_prediction
 from Investigations.DL_CNN.test_model import test_model_accuracy
 
 from pathlib import Path
 
 
-def run_analysis_set_1(model_path):
-    save_base_dir = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Analysis'
+def run_test_set(model_path, save_directory):
 
-    save_directory_1 = f'{save_base_dir}/Engine Hex Static 3/{Path(model_path).stem}'
-    Path(save_directory_1).mkdir(exist_ok=True, parents=True)
+    Path(save_directory).mkdir(exist_ok=True, parents=True)
 
-    base_path_1 = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Data'
-
-    directory_list = [
-        f'{base_path_1}/ML Model Data/Static Test 3/testing',
-    ]
-
+    base_path = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Data'
+    directory_list = f'{base_path}/ML Model Data/Static Test 3/testing',
     chunk_type = ['regular', 'window']
-    for path in directory_list:
-        test_model_accuracy(model_path, path, chunk_type[1], save=True, save_path=save_directory_1)
+
+    test_model_accuracy(model_path, directory_list, chunk_type[0], save=True, save_path=save_directory)
 
 
 if __name__ == '__main__':
     base_path = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Acoustic_Py'
-    model_path = Path(f'{base_path}/Detection_Classification/Static_3_Exp/model_library')
+    save_base_dir = '/Users/KevMcK/Dropbox/2 Work/1 Optics Lab/1 Acoustic/Analysis'
 
-    # run_analysis_set_1(model_path)
+    # Make sure to change TEMPLATE for your current project
+    model_path = Path(f'{base_path}/Detection_Classification/TEMPLATE/model_library')
+    save_directory = f'{save_base_dir}/TEMPLATE/{Path(model_path).stem}'
 
     for model in model_path.iterdir():
         if 'h5' in str(model):
-            run_analysis_set_1(model.resolve())
+            run_test_set(model.resolve(), save_directory)
