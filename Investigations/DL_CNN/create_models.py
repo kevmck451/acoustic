1df1166282b217b9dfe2f4f16547aca138dfd0f7
KevMcK <kmcrunk@gmail.com>
2024-01-30 15:22:59 -0600

    added possible new method

--- Investigations/DL_CNN/create_models.py
+++ Investigations/DL_CNN/create_models.py
@@ -5,8 +5,62 @@ from keras.layers import Dense, Dropout, Conv2D, Flatten, MaxPooling2D
 from keras.layers import BatchNormalization
 from keras.models import Sequential
 from keras.regularizers import l2
+import io
 
 
+# def create_model(input_shape, conv_layers, dense_layers, l2_value=0.01, dropout_rate=0.5, activation='relu'):
+#     """
+#     Parameters:
+#     - input_shape: tuple, shape of the input data (height, width, channels)
+#     - conv_layers: list of tuples, each tuple has (number of filters, kernel size)
+#     - dense_layers: list of integers, number of neurons in each dense layer
+#     - dropout_rate: dropout rate for regularization
+#     - activation: activation function to use
+#
+#     Returns: model: Keras Sequential model
+#     """
+#     print('Creating Model')
+#     model = Sequential()
+#
+#     # First convolutional layer with input shape
+#     filters, kernel_size = conv_layers[0]
+#     model.add(Conv2D(
+#         filters=filters,
+#         kernel_size=kernel_size,
+#         activation=activation,
+#         input_shape=input_shape,
+#         padding='same',
+#         kernel_regularizer=l2(l2_value)))
+#     # Consider removing or using smaller pooling layers
+#     model.add(MaxPooling2D(pool_size=(2, 1)))
+#     model.add(Dropout(dropout_rate))
+#
+#     # Adding subsequent convolutional layers
+#     for filters, kernel_size in conv_layers[1:]:
+#         model.add(Conv2D(filters=filters, kernel_size=kernel_size, activation=activation, padding='same'))
+#         # Consider reducing the frequency of MaxPooling layers
+#         # model.add(MaxPooling2D(pool_size=(2, 1)))
+#         model.add(Dropout(dropout_rate))
+#
+#     # Flatten the features for dense layers
+#     model.add(Flatten())
+#
+#     # Adding dense layers with dropout for regularization
+#     for units in dense_layers:
+#         model.add(Dense(units, activation=activation))
+#         model.add(Dropout(dropout_rate))
+#
+#     # Final layer with sigmoid activation for binary classification
+#     model.add(Dense(1, activation='sigmoid'))
+#
+#     # model.summary()
+#
+#     return model
+
+
+
+
+# base method for creating models
 def create_model(input_shape, conv_layers, dense_layers, l2_value=0.01, dropout_rate=0.5, activation='relu'):
     """
     Parameters:
@@ -30,10 +84,7 @@ def create_model(input_shape, conv_layers, dense_layers, l2_value=0.01, dropout_
     # Adding subsequent convolutional layers
     for filters, kernel_size in conv_layers[1:]:
         model.add(Conv2D(filters=filters, kernel_size=kernel_size, activation=activation))
-        try:
-            model.add(MaxPooling2D(pool_size=(2, 2)))
-        except:
-            model.add(MaxPooling2D(pool_size=(2, 1)))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(dropout_rate))
 
     # Flatten the features for dense layers
@@ -47,7 +98,7 @@ def create_model(input_shape, conv_layers, dense_layers, l2_value=0.01, dropout_
     # Final layer with sigmoid activation for binary classification
     model.add(Dense(1, activation='sigmoid'))
 
-    # model.summary()
+    model.summary()
 
     return model
 
